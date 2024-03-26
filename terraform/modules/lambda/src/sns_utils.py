a6750e3d7a8633037d6f02c34a0c5147e91b5700
atamakonkurii <morisudesu@gmail.com>
2024-03-26 22:21:15 +0900

    軽微な修正

--- terraform/modules/lambda/src/sns_utils.py
+++ terraform/modules/lambda/src/sns_utils.py
@@ -1,12 +1,12 @@
 import boto3
 
 from secret_manager_utils import SecretManagerUtils
-from constants import REGION
+from constants import REGION, SNS_TOPIC_ARN_SECRET_NAME
 
 class SNSUtils:
     def __init__(self):
         self.sns = boto3.client('sns')
-        secret_manager_sns_topic_arn = SecretManagerUtils("SNS_TOPIC_ARN", REGION)
+        secret_manager_sns_topic_arn = SecretManagerUtils(SNS_TOPIC_ARN_SECRET_NAME, REGION)
         sns_topic_arn = secret_manager_sns_topic_arn._get_secret()
         self.topic_arn = sns_topic_arn
 
